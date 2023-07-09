13781f4889b52a50275fb6068bc12ea4f50246aa
Abdullah YANIK <abdullahyanikiletisim@gmail.com>
2023-07-09 22:55:36 +0300

    Version 1.0
    Project uploaded

---
+++ core/config.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+define("DB_SERVER","localhost");
+define("DB_NAME","db_name"); //change info
+define("DB_USER","db_username"); //change info
+define("DB_PASS","db_userpass"); //change info
+
+define("APP_VERSION", 1);
+
+define("SITE_URL","yourwebsiteurl"); //change info
+
+define("LICENSE_KEY","FRQIZ-IESZT-WVGEB-JACXZ");
+
+?>
